78c5a4b051d669b1d6e0dfe8aef018564981111e
Ryse <henryfabianguillen@gmail.com>
2026-04-15 07:17:43 -0500

    fc

---
+++ src/main/java/com/techsolution/TechsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.techsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechsolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechsolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsolution/TechsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
